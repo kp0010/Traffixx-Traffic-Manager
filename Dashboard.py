04efd599e45e7d427de1bf8cbd85690a43282689
kp0010 <122145240+kp0010@users.noreply.github.com>
2024-04-02 11:49:21 +0530

    Dashboard Update

--- Dashboard.py
+++ Dashboard.py
@@ -38,23 +38,21 @@ class Dashboard(tk.Frame):
         row1_y = .27
         row2_y = .73
 
+        rel_positions = [(col1_x, row1_y), (col2_x, row1_y), (col1_x, row2_y), (col2_x, row2_y)]
 
-        rel_positions = [(col1_x, row1_y), (col1_x, row2_y), (col2_x, row1_y), (col2_x, row2_y)]
-        
         # Player 1
 
         p1pos = rel_positions[0]
 
         p1frame = tk.Frame(master=self, bg=BGCOLOR, height=pl_height, width=pl_width)
-        p1frame.place(relx=p1pos[0], rely=p1pos[1],
-                      relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
+        p1frame.place(relx=p1pos[0], rely=p1pos[1], relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
 
         player1 = TkinterVideo(master=p1frame, bg=BGCOLOR, height=800, width=390)
         player1.load(VIDEOS[0])
         player1.set_size((pl_width, pl_height))
         player1.play()
 
-        def loop1(e):
+        def loop1(_):
             player1.play()
 
         player1.bind("<<Ended>>", loop1)
@@ -65,8 +63,7 @@ class Dashboard(tk.Frame):
         p2pos = rel_positions[1]
 
         p2frame = tk.Frame(master=self, bg=BGCOLOR, height=pl_height, width=pl_width)
-        p2frame.place(relx=p2pos[0], rely=p2pos[1],
-                      relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
+        p2frame.place(relx=p2pos[0], rely=p2pos[1], relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
 
         player2 = TkinterVideo(master=p2frame, bg=BGCOLOR, height=800, width=390)
         player2.load(VIDEOS[1])
@@ -79,8 +76,7 @@ class Dashboard(tk.Frame):
         p3pos = rel_positions[2]
 
         p3frame = tk.Frame(master=self, bg=BGCOLOR, height=pl_height, width=pl_width)
-        p3frame.place(relx=p3pos[0], rely=p3pos[1],
-                      relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
+        p3frame.place(relx=p3pos[0], rely=p3pos[1], relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
 
         player3 = TkinterVideo(master=p3frame, bg=BGCOLOR, height=800, width=390)
         player3.load(VIDEOS[3])
@@ -93,8 +89,7 @@ class Dashboard(tk.Frame):
         p4pos = rel_positions[3]
 
         p4frame = tk.Frame(master=self, bg=BGCOLOR, height=pl_height, width=pl_width)
-        p4frame.place(relx=p4pos[0], rely=p4pos[1],
-                      relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
+        p4frame.place(relx=p4pos[0], rely=p4pos[1], relwidth=rel_size, relheight=rel_size, anchor=tk.CENTER)
 
         player4 = TkinterVideo(master=p4frame, bg=BGCOLOR, height=800, width=390)
         player4.load(VIDEOS[2])
@@ -113,16 +108,52 @@ class Dashboard(tk.Frame):
                 element.play()
 
         play_all_btn = tk.Button(text="Play All", command=play_all)
-        play_all_btn.place(x=1222, y=80, relheight=0.05, relwidth=0.19)
+        play_all_btn.place(relx=0.793, rely=0.095, relheight=0.05, relwidth=0.19)
 
         def pause_all():
             for element in players:
                 element.pause()
 
         pause_all_btn = tk.Button(text="Pause All", command=pause_all)
-        pause_all_btn.place(x=1222, y=152, relheight=0.05, relwidth=0.19)
+        pause_all_btn.place(relx=0.793, rely=0.15, relheight=0.05, relwidth=0.19)
 
-        self.window.after(200, pause_all)
+        self.window.after(110, pause_all)
+
+        pause_vars = [tk.BooleanVar() for _ in range(4)]
+
+        pause_1_chkbtn = tk.Checkbutton(root, text="Pause 1", onvalue=True, offvalue=False, height=2, width=10,
+                                        variable=pause_vars[0])
+        pause_1_chkbtn.place(relx=0.793, rely=0.205, relwidth=0.045)
+
+        pause_2_chkbtn = tk.Checkbutton(root, text="Pause 2", onvalue=True, offvalue=False, height=2, width=10,
+                                        variable=pause_vars[1])
+        pause_2_chkbtn.place(relx=0.8415, rely=0.205, relwidth=0.045)
+
+        pause_3_chkbtn = tk.Checkbutton(root, text="Pause 3", onvalue=True, offvalue=False, height=2, width=10,
+                                        variable=pause_vars[2])
+        pause_3_chkbtn.place(relx=0.89, rely=0.205, relwidth=0.045)
+
+        pause_4_chkbtn = tk.Checkbutton(root, text="Pause 4", onvalue=True, offvalue=False, height=2, width=10,
+                                        variable=pause_vars[3])
+        pause_4_chkbtn.place(relx=0.9385, rely=0.205, relwidth=0.045)
+
+
+        def pause_selective():
+            for var, player in zip(pause_vars, players):
+                if var.get():
+                    player.pause()
+
+
+        pause_selective_btn = tk.Button(text="Pause Selected", command=pause_selective)
+        pause_selective_btn.place(relx=0.793, rely=0.25, relwidth=0.19, relheight=0.05)
+
+        def play_selective():
+            for var, player in zip(pause_vars, players):
+                if var.get():
+                    player.play()
+
+        play_selective_btn = tk.Button(text="Play Selected", command=play_selective)
+        play_selective_btn.place(relx=0.793, rely=0.305, relwidth=0.19, relheight=0.05)
 
 
 if __name__ == "__main__":
