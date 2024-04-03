e28dd7856f5a11486f32797f9abbc5d89139825d
KP0010 <kartikcr750@gmail.com>
2024-04-03 14:45:03 +0530

    Forgot Password Int

--- database.py
+++ database.py
@@ -1,8 +1,6 @@
-import os
-
 import sqlalchemy
 from sqlalchemy.orm import declarative_base, Session
-from sqlalchemy import Column, String, select
+from sqlalchemy import Column, String, select, update
 from werkzeug.security import generate_password_hash, check_password_hash
 
 SQLURI = "sqlite:///users"
@@ -35,7 +33,7 @@ class Database:
 
     def add_new_user(self, userid, name, password, phone, email):
         with Session(self.engine) as sesh:
-            hashed_pass = generate_password_hash(password, method="pbkdf2:sha256", salt_length=10)
+            hashed_pass = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
             new_user = User(id=userid, name=name, password=hashed_pass, phone=phone, email=email)
 
             sesh.add(new_user)
@@ -54,13 +52,35 @@ class Database:
             else:
                 return False
 
+    def forgot_password(self, userid, oldpass, newpass):
+
+        newpass_hashed = generate_password_hash(password=newpass, method="pbkdf2:sha256", salt_length=8)
+
+        with Session(self.engine) as sesh:
+            sel_user = self.check_user_cred(userid, oldpass)
+
+            if sel_user is None:
+                return None
+
+            elif sel_user:
+                sesh.execute(update(User).where(User.id == userid).values(password=newpass_hashed))
+
+                sesh.commit()
+
+                return True
+
+            else:
+                return False
+
 
 if __name__ == '__main__':
     db = Database(echo=False)
 
-    # db.add_new_user("KP0011", "Kartikkk", "asjdflksjdf", phone="9230423234", email="kasdfas@gmail.com")
+    # db.add_new_user("KP001", "Kartikkk", "kartikcrs", phone="2234232342", email="kasd23423fas@gmail.com")
 
     with Session(db.engine) as session:
         user = session.execute(select(User).filter_by(id="KP0011")).scalar_one_or_none()
-        print(user)
+        # print(user)
         session.commit()
+
+    db.forgot_password("KP001", "kartikcr7", "kartikcrs")
